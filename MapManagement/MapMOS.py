7d71b2c0e6c7653b8fa0bd735ee3e5e2678b7eda
yoonseon-oh <41294373+yoonseon-oh@users.noreply.github.com>
2021-07-09 15:01:43 +0900

    Add MapMOS: parse the map.txt

---
+++ MapManagement/MapMOS.py
@@ -0,0 +1,65 @@
+import sys, os, io
+
+class MapMOS:
+    def __init__(self,mapfile):
+        # initialize variables
+
+        self.VertexNames = []
+        self.VertexType = {} # vertex_name: type (0:move, 1: station, 2: charging)
+        self.VertexPos = {} # vertex_name: pos [x,y,z]
+        self.Edge = {} # vertex_name: [vertex_name, vertex_name, vertex_name]
+
+        self.EdgeMove = {} # A robot can move by a moving function
+        self.EdgeLoad = {}  # A robot can move by a loading function
+        self.EdgeUnload = {}  # A robot can move by a unloading function
+        self.EdgeCharge = {}  # A robot can move by a charging function
+
+        self.readmapfile(mapfile)
+
+    def readmapfile(self, mapfile):
+        f = open(mapfile, 'r')
+        lines = f.readlines()
+        lnum = 0 # line number
+        vname = ""
+        type = -1
+        pos = []
+        edges = []
+
+        while lnum < len(lines):
+            if lines[lnum] == 'Vertex\n':
+                if not vname == "":
+                    self.VertexNames.append(vname)
+                    self.VertexType[vname] = type
+                    self.VertexPos[vname] = pos
+                    self.Edge[vname] = edges
+
+                vname = ""
+                type = -1
+                pos = []
+                edges = []
+                lnum = lnum + 1
+
+            if 'name' in lines[lnum]:
+                vname = lines[lnum].split(" ")[1].split('\n')[0]
+                lnum = lnum +1
+
+            if 'type' in lines[lnum]:
+                type = lines[lnum].split(" ")[1].split('\n')[0]
+                lnum = lnum + 1
+
+            if 'pos' in lines[lnum]:
+                pos = [float(a) for a in lines[lnum].split(" ")[1:]]
+                lnum = lnum+1
+
+            if 'edge' in lines[lnum]:
+                edges.append(lines[lnum].split(" ")[1].split('\n')[0])
+                lnum = lnum + 1
+
+        f.close()
+
+
+
+if __name__=="__main__":
+    map = MapMOS("../data/map_cloud.txt")
+    print("done")
+
